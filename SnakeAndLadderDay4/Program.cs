568c0e7033b37b7c2c710eca3554a0e901845974
Divya <beingdivyadixit@gmail.com>
2022-12-22 21:00:51 +0530

    for loop until he reaches on 100 position(wins) UC4

--- SnakeAndLadderDay4/Program.cs
+++ SnakeAndLadderDay4/Program.cs
@@ -9,25 +9,30 @@ namespace SnakeAndLadderDay4
             Console.WriteLine("Begin Game");
             int Position = 0; 
             Random random= new Random();
-            int DiceRoll = random.Next(1,7);  //Dice roll using random method
-            int Option = random.Next(0, 3);  // Checking option by using random method
+
             //taking option by using switch case
-            switch (Option)
+            while (Position<=100)
             {
-                case 0:
-                    Position = Position + DiceRoll;  // Updating position
-                    Console.WriteLine("Player got the Ladder and the dice : {0} Now player is in the position : {1}",DiceRoll,Position);
-                    break;
-                case 1:
-                    Position = Position - DiceRoll; //Updating position
-                    if (Position < 0)
-                        Position = 0;
-                    Console.WriteLine("Player got the Snake and the dice : {0} Now player is in the position : {1}", DiceRoll, Position);
-                    break;
-                default:
-                    Console.WriteLine(" No play , the dice :{0} & the player stays in same position : {} ", DiceRoll, Position);
-                    break;
-          }
+                int DiceRoll = random.Next(1, 7);
+                int Option = random.Next(0, 3);  // Checking option by using random method
+                  //Dice roll using random method
+                switch (Option)
+                {
+                    case 0:
+                        Position = Position + DiceRoll;  // Updating position
+                        Console.WriteLine("Player got the Ladder and the dice : {0} Now player is in the position : {1}" , DiceRoll, Position);
+                        break;
+                    case 1:
+                        Position = Position - DiceRoll; //Updating position
+                        if (Position < 0)
+                           Position = 0;
+                        Console.WriteLine("Player got the Snake and the dice : {0} Now player is in the position : {1}" , DiceRoll, Position);
+                        break;
+                    default:
+                        Console.WriteLine("No play,the dice : {0} & the player stays in same position : {1} ", DiceRoll, Position);
+                        break;
+                }
+            }
 
         }
     }
