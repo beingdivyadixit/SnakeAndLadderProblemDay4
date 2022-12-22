a13f98aaf76fb00fa9554796b3c97c08dd237079
Divya <beingdivyadixit@gmail.com>
2022-12-22 20:14:04 +0530

    Rolls The Die UC2

--- SnakeAndLadderDay4/Program.cs
+++ SnakeAndLadderDay4/Program.cs
@@ -8,7 +8,10 @@ namespace SnakeAndLadderDay4
         {
             Console.WriteLine("Begin Game");
             int Position = 0; 
-            Console.WriteLine("Player is in Position {0}",Position); //Player is on 0th Position
+            Random random= new Random();
+            int DiceRoll = random.Next(1,7); //Dice roll using random method  
+            Console.WriteLine("Player is in Position {0} & Dice After Roll is {1}", Position, DiceRoll);
+
         }
     }
 }
