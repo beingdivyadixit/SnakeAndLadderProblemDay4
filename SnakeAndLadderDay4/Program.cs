57e3652f355f517f48aa369f112d1d8a130ebf00
Divya <beingdivyadixit@gmail.com>
2022-12-22 19:55:05 +0530

    Begin Game

---
+++ SnakeAndLadderDay4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadderDay4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin Game");
+        }
+    }
+}
