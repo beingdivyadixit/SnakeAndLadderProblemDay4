cd169da66b2561aaedae2b98874c4c0cd6970b9a
Divya <beingdivyadixit@gmail.com>
2022-12-22 20:31:54 +0530

    Checking For Options UC3

--- SnakeAndLadderDay4/Program.cs
+++ SnakeAndLadderDay4/Program.cs
@@ -9,8 +9,25 @@ namespace SnakeAndLadderDay4
             Console.WriteLine("Begin Game");
             int Position = 0; 
             Random random= new Random();
-            int DiceRoll = random.Next(1,7); //Dice roll using random method  
-            Console.WriteLine("Player is in Position {0} & Dice After Roll is {1}", Position, DiceRoll);
+            int DiceRoll = random.Next(1,7);  //Dice roll using random method
+            int Option = random.Next(0, 3);  // Checking option by using random method
+            //taking option by using switch case
+            switch (Option)
+            {
+                case 0:
+                    Position = Position + DiceRoll;  // Updating position
+                    Console.WriteLine("Player got the Ladder and the dice : {0} Now player is in the position : {1}",DiceRoll,Position);
+                    break;
+                case 1:
+                    Position = Position - DiceRoll; //Updating position
+                    if (Position < 0)
+                        Position = 0;
+                    Console.WriteLine("Player got the Snake and the dice : {0} Now player is in the position : {1}", DiceRoll, Position);
+                    break;
+                default:
+                    Console.WriteLine(" No play , the dice :{0} & the player stays in same position : {} ", DiceRoll, Position);
+                    break;
+          }
 
         }
     }
