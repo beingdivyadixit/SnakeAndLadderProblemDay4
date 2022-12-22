e38deb002a81c688396ac8c1be313a6d1d0db515
Divya <beingdivyadixit@gmail.com>
2022-12-22 20:04:36 +0530

    Player is on 0th Position UC1

--- SnakeAndLadderDay4/Program.cs
+++ SnakeAndLadderDay4/Program.cs
@@ -7,6 +7,8 @@ namespace SnakeAndLadderDay4
         static void Main(string[] args)
         {
             Console.WriteLine("Begin Game");
+            int Position = 0; 
+            Console.WriteLine("Player is in Position {0}",Position); //Player is on 0th Position
         }
     }
 }
